881e3a24de99bb1ec8c643e32a6790178405a05c
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-12 10:22:39 +0530

    [AR] Exception with Custom Class

--- ExceptionHandling/MoodAnalyzer.cs
+++ ExceptionHandling/MoodAnalyzer.cs
@@ -39,6 +39,25 @@ namespace ExceptionHandling
                 return "Happy";
             }
         }
-
+        public string AnalyseMood_withCustomException(string message)
+        {
+            try
+            {
+                if (this.message.Equals(""))
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                if (this.message.Contains("Sad"))
+                    return "Sad";
+                else
+                    return "Happy";
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Exception Handling");
+        }
     }
 }
---
+++ ExceptionHandling/MoodAnalyzerCustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace ExceptionHandling
+{
+    public class MoodAnalyzerCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE
+        }
+        public readonly ExceptionType type;
+        public MoodAnalyzerCustomException(ExceptionType Type, string message):base(message)
+        {
+            this.type = Type;
+        }
+        
+    }
+}
